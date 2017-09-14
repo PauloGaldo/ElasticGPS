67127a409b11cab72bf3a79668ccfa64afeea5fd
Paulo Galdo <paulo.galdo@softlogia.com>
2017-09-14 01:22:03 -0300

    Configuracion de mapa Evento para cargar nuevos puntos en GPS

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -1,14 +1,60 @@
 (function () {
     'use strict';
-    
+
     angular
             .module('ElasticGpsApp')
             .controller('MainCtrl', MainCtrl);
-    
-    MainCtrl.$inject = [];
-    
-    function MainCtrl() {
+
+    MainCtrl.$inject = ['$scope', 'uiGmapGoogleMapApi', 'ElasticService', 'MapFactory'];
+
+    function MainCtrl($scope, uiGmapGoogleMapApi, ElasticService, MapFactory) {
+        var vm = this;
+        /*VARIABLES*/
+        vm.map = {
+            center: {latitude: 20, longitude: -100}, zoom: 5,
+            markers: [],
+            options: {
+                streetViewControl: false,
+                mapTypeControl: false,
+                scaleControl: true,
+                rotateControl: true,
+                zoomControl: true
+            },
+            showTraficLayer: true
+        };
+        vm.markerControl = {};
+        /*METODOS Y/O FUNCIONES*/
+        vm.refreshMap = refreshMap;
         
+        /**
+         * Evento producido al terminar de cargar contenido de vista
+         * @param {type} event 
+         */
+        $scope.$on('$viewContentLoaded', function (event) {
+            uiGmapGoogleMapApi.then(function (maps) {
+                maps.visualRefresh = true;
+                refreshMap();
+            });
+        });
+
+        /**
+         * Funcion para actualizar valores en mapa
+         * @returns {undefined}
+         */
+        function refreshMap() {
+            ElasticService
+                    .getGpsData()
+                    .then(function (response) {
+                        console.log(response);
+                        var locations = MapFactory.buildMarkers(response.data.hits.hits);
+                        console.log(locations);
+                        vm.map.center = locations.center;
+                        vm.map.markers = locations.markers;
+                        vm.map.zoom = 15;
+                    });
+        }
+
+
     }
-    
+
 })();
