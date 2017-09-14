3708283d9c7670925e3de1f52123f5ab796b5b72
Paulo Galdo <paulo.galdo@softlogia.com>
2017-09-14 00:31:34 -0300

    Maps inicializado State agregado

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -5,13 +5,21 @@
             .module('ElasticGpsApp')
             .config(Configuration);
 
-    Configuration.$inject = ['$stateProvider', '$urlRouterProvider'];
+    Configuration.$inject = ['$stateProvider', '$urlRouterProvider', 'uiGmapGoogleMapApiProvider'];
 
-    function Configuration($stateProvider, $urlRouterProvider) {
+    function Configuration($stateProvider, $urlRouterProvider, uiGmapGoogleMapApiProvider) {
+
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyBclVIaszEyDfUuuk2iFVgpRcJq2gcFCAQ',
+            v: '3.27',
+            libraries: 'weather,geometry,visualization'
+        });
+
         $stateProvider
                 .state('main', {
                     url: '/',
-                    templateUrl: 'views/map.html'
+                    templateUrl: 'views/map.html',
+                    controller: 'MainCtrl as vm'
                 });
         $urlRouterProvider.otherwise('/');
     }
