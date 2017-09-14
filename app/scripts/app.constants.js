5b43954f23000c3e4540dc6a462bc8a413f6db2f
Paulo Galdo <paulo.galdo@softlogia.com>
2017-09-14 00:07:06 -0300

    Constante definida

---
+++ app/scripts/app.constants.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strcit';
+
+    angular
+            .module('ElasticGpsApp')
+            .constant('Constant', {
+                blueicon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                redicon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            });
+
+})();
